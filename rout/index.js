1303f1d4660dbc56744a11109056146ac915f976
draaaaaaaaaaaaaaa <moh.daffa.ucui@gmail.com>
2022-03-31 10:59:42 +0700

    comit

--- rout/index.js
+++ rout/index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+
 app.use("/",require("./routes/index"));
+
 //disini routing
 app.listen(process.env.PORT, () =>{
     console.log(`Server berjalan di port ${process.env.PORT}`);
--- rout/rouTEs/index.js
+++ rout/rouTEs/index.js
@@ -1,3 +1,22 @@
+const users =[
+    {
+        id : 1, 
+        nama : "da",
+        kelas : "Rpla"
+    },
+    {
+        id: 2,
+        nama: "dz",
+        kelas : "RPB"
+    },
+    {
+        id:3,
+        nama:"dm",
+        kelas : "Rpc"
+    }
+
+]
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
@@ -10,7 +29,7 @@ router.get("/", (req, res, next) => {
     res.send('Goodbye, Mars!')
 })
 
-
+//method post
 router.post(
     "/",
     bodyParser.urlencoded({
@@ -21,6 +40,28 @@ router.post(
          }
 )
 
+//method delete
+router.delete("/id", (req,res,send)=>{
+    res.send(`data dengan ${req.params.id} dihapus`)
+})
+
+router.delete("/", (req,res,next)=>{
+    res.send(`Tidak dapat menghapus , karena anda belum menambahkan ID`)
+})
+
+router.get("/user",(req,res,next)=>{
+    res.send({users})
+})
+
+router.get('/user/:id', (req, res, next) => {
+    // hello user
+    const user = users.find(({id}) => id === parseInt(req.params.id))
+    if (!user) {
+        res.status(404).send('user not found')
+    }
+    res.send(user)
+})
+        
 
 
 module.exports = router
